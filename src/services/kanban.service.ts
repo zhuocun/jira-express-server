7f0203d36296faefe145c6b9bb11e2904096524a
zhuocun <zhuocun.tang@outlook.com>
2022-11-28 20:19:11 +1100

    feat: initiate kanban

--- src/services/kanban.service.ts
+++ src/services/kanban.service.ts
@@ -7,8 +7,27 @@ import projectModel from "../models/project.model.js";
 const get = async (req: Request, res: Response) => {
     const { projectId } = req.query;
     if (projectId) {
-        const kanban = await kanbanModel.findOne({ projectId });
-        res.status(StatusCode.OK).json(kanban);
+        const project = await projectModel.findById(projectId);
+        if (project) {
+            const kanbans = await kanbanModel.find({ projectId });
+            if (!kanbans.length) {
+                await kanbanModel.create({
+                    kanbanName: "To Do",
+                    projectId: projectId as string
+                });
+                await kanbanModel.create({
+                    kanbanName: "In Progress",
+                    projectId: projectId as string
+                });
+                await kanbanModel.create({
+                    kanbanName: "Done",
+                    projectId: projectId as string
+                });
+            }
+            res.status(StatusCode.OK).json(await kanbanModel.find({ projectId }));
+        } else {
+            res.status(StatusCode.NOT_FOUND).json("Project not found");
+        }
     } else {
         res.status(StatusCode.NOT_FOUND).json("Kanban not found");
     }
