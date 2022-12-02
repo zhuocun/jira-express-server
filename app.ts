d423f921fe88ff4980705d2bb8aa0cca23c8d18f
zhuocun <zhuocun.tang@outlook.com>
2022-12-02 19:57:46 +1100

    chore: config vercel env

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import morgan from "morgan";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-import router from "./src/routes/index.route.js";
+import router from "./routes/index.route.js";
 
 dotenv.config();
 
