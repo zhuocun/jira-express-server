e5d0a5ce67d9248644bb479023bf568331267c14
zhuocun <zhuocun.tang@outlook.com>
2023-04-08 19:49:42 +1000

    chore: code format

--- utils/dynamo.util.ts
+++ utils/dynamo.util.ts
@@ -3,13 +3,17 @@ import { CreateTableCommandInput } from "@aws-sdk/client-dynamodb";
 
 const createDynamoDBTable = (params: CreateTableCommandInput): void => {
     dynamoDB.describeTable(params, (err) => {
-        const tableName: string = params.TableName != null ? params.TableName : "";
+        const tableName: string =
+            params.TableName != null ? params.TableName : "";
         if (err != null) {
             if (err.code === "ResourceNotFoundException") {
                 // Table does not exist, create it
                 dynamoDB.createTable(params, (err) => {
                     if (err != null) {
-                        console.error(`Error creating table ${tableName}:`, err);
+                        console.error(
+                            `Error creating table ${tableName}:`,
+                            err
+                        );
                     } else {
                         console.log(`Created table ${tableName}.`);
                     }
