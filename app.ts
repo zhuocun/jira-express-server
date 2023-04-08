2df59a060d760bb6493b66caba678bb118ec30f7
zhuocun <zhuocun.tang@outlook.com>
2023-04-08 18:26:43 +1000

    feat: initialise dynamoDB

--- app.ts
+++ app.ts
@@ -3,7 +3,10 @@ import cors from "cors";
 import morgan from "morgan";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
+import AWS from "aws-sdk";
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
 import router from "./routes/index.route.js";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 
 const app = express();
 dotenv.config();
@@ -23,8 +26,11 @@ const main = async (): Promise<void> => {
     await mongoose.connect(
         process.env.MONGO_URI != null ? process.env.MONGO_URI : ""
     );
+    AWS.config.update({ accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY });
 };
 
+const dynamoDBDocument = DynamoDBDocument.from(new DynamoDB({ region: process.env.AWS_REGION }));
+const dynamoDB = new DynamoDB({ region: process.env.AWS_REGION });
 main()
     .then(() => {
         console.log("Connect to MongoDB successfully.");
@@ -33,3 +39,5 @@ main()
         console.log(err);
         console.log("Connect to MongoDB failed.");
     });
+
+export { dynamoDB, dynamoDBDocument };
---
+++ dynamoDB.ts
@@ -0,0 +1,28 @@
+import { dynamoDB } from "./app.js";
+
+const userTableParams = {
+    TableName: "User",
+    KeySchema: [
+        { AttributeName: "username", KeyType: "HASH" },
+        { AttributeName: "email", KeyType: "RANGE" }
+    ],
+    AttributeDefinitions: [
+        { AttributeName: "username", AttributeType: "S" },
+        { AttributeName: "email", AttributeType: "S" }
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 5,
+        WriteCapacityUnits: 5
+    },
+    StreamSpecification: {
+        StreamEnabled: false
+    }
+};
+
+dynamoDB.createTable(userTableParams, (err: any, data: any) => {
+    if (err != null) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});
