4957595ac1603bf18599e0986531dcbbf73943fc
zhuocun <zhuocun.tang@outlook.com>
2023-04-12 11:49:24 +1000

    feat: create in postgreSQL

--- utils/database/CRUD/create.ts
+++ utils/database/CRUD/create.ts
@@ -1,6 +1,6 @@
 import EDatabase from "../../../constants/eDatabase.js";
 import EError from "../../../constants/eError.js";
-import { database, dynamoDBDocument } from "../../../database.js";
+import { database, dynamoDBDocument, postgresPool } from "../../../database.js";
 import userModel from "../../../models/user.model.js";
 import projectModel from "../../../models/project.model.js";
 import { v4 } from "uuid";
@@ -8,7 +8,7 @@ import ETableName from "../../../constants/eTableName.js";
 import taskModel from "../../../models/task.model.js";
 import columnModel from "../../../models/column.model.js";
 
-const createMongoDB = async <P>(
+const createItemMongoDB = async <P>(
     reqBody: P,
     tableName: string
 ): Promise<void> => {
@@ -30,7 +30,7 @@ const createMongoDB = async <P>(
     }
 };
 
-const createDynamoDB = async <P>(
+const createItemDynamoDB = async <P>(
     reqBody: P,
     tableName: string
 ): Promise<void> => {
@@ -44,13 +44,30 @@ const createDynamoDB = async <P>(
     });
 };
 
+const createItemPostgreSQL = async <P>(
+    reqBody: P,
+    tableName: string
+): Promise<void> => {
+    const keys = Object.keys(reqBody as Record<string, any>);
+    const values = Object.values(reqBody as Record<string, any>);
+
+    const queryParams = keys.map((_, index) => `$${index + 1}`).join(", ");
+    // query = INSERT INTO tableName (key1, key2, key3) VALUES ($1, $2, $3)
+    const query = `INSERT INTO ${tableName} (${keys.join(", ")}) VALUES (${queryParams})`;
+
+    await postgresPool.query(query, values);
+};
+
 const createItem = async <P>(reqBody: P, tableName: string): Promise<void> => {
     switch (database) {
+        case EDatabase.POSTGRESQL:
+            await createItemPostgreSQL(reqBody, tableName);
+            break;
         case EDatabase.DYNAMODB:
-            await createDynamoDB(reqBody, tableName);
+            await createItemDynamoDB(reqBody, tableName);
             break;
         case EDatabase.MONGODB:
-            await createMongoDB(reqBody, tableName);
+            await createItemMongoDB(reqBody, tableName);
             break;
         default:
             throw new Error(EError.INVALID_DB);
